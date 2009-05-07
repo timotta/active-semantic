1b1fdc7c2de54c535a0dd8ee301b2fe2a4752082
Tiago Motta <tiago.motta@dl128143.(none)>
2009-05-06 21:18:40 -0300

    Soh a estrutura

---
+++ lib/semantic_entities.rb
@@ -0,0 +1,3 @@
+module SematicEntities
+  
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+$:.unshift "#{File.dirname(__FILE__)}/../lib"
+
+require 'test/unit'
+require 'open-uri'
+require 'fake_web'
+require 'rubygems'
+require 'mocha'
